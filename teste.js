52a01bb62de7d570d9a3c9c4213f8c2827dbc54c
Timaista <filipe.rdoc@gmail.com>
2026-04-24 11:27:40 -0300

    sim

--- teste.js
+++ teste.js
@@ -1,4 +1,11 @@
+const menssage = "Hello World"
+console.log(menssage)
+
 let nota1 = 9;
 let nota2 = 7;
 let media = (nota1 + nota2) / 2; 
 console.log("A média do aluno é: "+media)
+
+let economias = 0;
+economias += (50 * 2) - 30;
+console.log("Valor final: " + economias)
