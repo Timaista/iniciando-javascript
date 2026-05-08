40314e8b93f94374eee1f4be11bef33bd8458ebf
Timaista <filipe.rdoc@gmail.com>
2026-05-08 11:31:16 -0300

    sdsds

--- teste.js
+++ teste.js
@@ -15,6 +15,19 @@ console.log("Valor final: " + economias)
 let temp = 32;
 console.log("Está quente? ", (temp > 30));
 
+let temmingresso = true
+let maiordeidade = true
+console.log("Pode entrar no show?", temmingresso && maiordeidade)
+
+let velocidade = 90
+if (velocidade <= 60) {
+    console.log("Dentro do limite.")
+} else if (velocidade <= 80) {
+    console.log("Atenção.")
+} else {
+    console.log("Mutado.")
+}
+
 let pontuacao = 100
 let resultado = pontuacao >= 100 ? "Você ganhou um prêmio!" : "Tente novamente"
 console.log(resultado)
