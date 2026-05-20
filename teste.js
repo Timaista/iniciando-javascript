995f71d237aab464653a88629ee716d8ead46af5
Timaista <213489450+Timaista@users.noreply.github.com>
2026-05-20 16:57:24 -0300

    sddsd

--- teste.js
+++ teste.js
@@ -49,8 +49,10 @@ while (contagem >=1) {
 }
 console.log("Contagem regressiva encerrada.")
 
-for (let i = 0; i <=10; i += 2) {
-    console.log(i)
+for (let i = 0; i <=10; i++) {
+    if(i%2 === 0){
+        console.log(i)
+    }
 }
 
 cursos = ['Curso de inglês', 'Curso de redação', 'Curso de programação']
